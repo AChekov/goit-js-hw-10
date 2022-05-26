724e14dd2bf44add60b22e8210a03071897a7db9
AChekov <4ekoff@gmail.com>
2022-05-26 23:31:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
+// import { fetchCountries } from './js/fetchCountries';
 import { getRefs } from './js/refs';
 import debounce from 'lodash.debounce';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
